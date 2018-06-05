7278bb5a617a045d0d6cd1b3435182b87ac5a065
zhaojiawei123 <fengtingyulai@163.com>
2018-06-05 18:32:56 +0800

    1
    1

--- AlienTrespass/alien_invasion.py
+++ AlienTrespass/alien_invasion.py
@@ -1,14 +1,17 @@
 
 import pygame,sys
 
+from settings import Settings
+
 def run_game():
-    #初始化游戏并创建一个屏幕对象
+    #初始化pygame、设置和屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((480,720))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_with,ai_settings.screen_height))
     pygame.display.set_caption("Alien Tnvasion")
 
-    #设置背景色
-    bg_color = (220,220,220)
+
     #开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
@@ -16,7 +19,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         #每次循环时都会重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
          #让最近绘制的屏幕可见
         pygame.display.flip()
 run_game()
